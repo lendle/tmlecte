884dd98358e89a9e774503af8b797ca4530117b9
Sam Lendle <sam.lendle@gmail.com>
2011-06-29 12:39:42 -0700

    Added tests for the regress and predict.regress functions

--- inst/tests/tests.R
+++ inst/tests/tests.R
@@ -7,3 +7,23 @@ test_that("qlogis and plogis work as I expect", {
   expect_that(logit(y), equals(log(y/(1-y))))
 })
 
+test_that("regress and predict.regress", {
+  X <- rnorm(100)
+  Y <- rnorm(100, 10+X, .1)
+  Ybin <- rbinom(100, 1, plogis(X))
+  expect_that(r1 <- regress(Y, X, family=gaussian), is_a("regress"))
+  expect_that(r2 <- regress(Ybin, X), is_a("regress"))
+  expect_that(predict(r1), is_a("numeric"))
+  expect_that(pr2 <- predict(r2), is_a("numeric"))
+  expect_that(all(pr2 >= 0 & pr2 <=1), is_true())
+  if (!("SuperLearner" %in% installed.packages()[,1L])) {
+    expect_that(rnsl <- regress(Y, X, method="SL", SL.library="glm"), gives_warning("SuperLearner is not installed"))
+    expect_that(rnsl$method, equals("glm"))
+  } else {
+    SL.version <- packageVersion("SuperLearner")$major
+    if (SL.version==1) {
+      expect_that(rsl <- regress(Y, X, method="SL", family=gaussian(), SL.library="SL.glm"), gives_warning("SuperLearner is out of date"))
+    }
+    expect_that(predict(rsl), is_a("matrix"))
+  }
+})
