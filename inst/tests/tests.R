5271f81deae4267068405565c67270ba8bc445f2
Sam Lendle <sam.lendle@gmail.com>
2011-07-01 23:14:52 -0700

    Removed logit and logistic from tests and replaced with qlogis and plogis

--- inst/tests/tests.R
+++ inst/tests/tests.R
@@ -3,8 +3,8 @@ context("Just some function checks")
 test_that("qlogis and plogis work as I expect", {
   x <- -10:10
   y <- seq(.1, .9, .1)
-  expect_that(logistic(x), equals(1/(1+exp(-x))))
-  expect_that(logit(y), equals(log(y/(1-y))))
+  expect_that(plogis(x), equals(1/(1+exp(-x))))
+  expect_that(qlogis(y), equals(log(y/(1-y))))
 })
 
 test_that("regress and predict.regress", {
