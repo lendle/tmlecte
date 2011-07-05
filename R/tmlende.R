2dc52eb2c5dadac160b6970581787168e8420409
Sam Lendle <sam.lendle@gmail.com>
2011-07-05 10:20:21 -0700

    Adding tmle.nde wrapper function

---
+++ R/tmlende.R
@@ -0,0 +1,8 @@
+tmle.nde <- function(A, WZ, Y, RCT=FALSE, ...) {
+  t <- tmle.cte(A, WZ, Y, a=0, ...)
+  t$estimand <- ifelse(RCT,
+                       "Natural direct effect",
+                       "Natural direct effect among the treated")
+  return(t)
+}
+
