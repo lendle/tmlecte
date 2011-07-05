175d769d7cf0fe8d88ed2b90b2dcc3f573e335f6
Sam Lendle <sam.lendle@gmail.com>
2011-07-05 14:52:10 -0700

    Fixed parameter name

--- R/tmlende.R
+++ R/tmlende.R
@@ -2,7 +2,7 @@ tmle.nde <- function(A, WZ, Y, RCT=FALSE, ...) {
   t <- tmle.cte(A, WZ, Y, a=0, ...)
   t$estimand <- ifelse(RCT,
                        "Natural direct effect",
-                       "Natural direct effect among the treated")
+                       "Natural direct effect among the untreated")
   return(t)
 }
 
