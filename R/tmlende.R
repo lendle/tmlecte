e90ef9b16c7d83ee6e2c7c6ad703a867c1e8d8d6
Sam Lendle <sam.lendle@gmail.com>
2011-07-06 13:49:40 -0700

    Added a little documentation for tmle.nde and exported the function.

--- R/tmlende.R
+++ R/tmlende.R
@@ -1,3 +1,15 @@
+##' <description>
+##'
+##' <details>
+##' @title <title>
+##' @param A treatment
+##' @param WZ baseline covariates and mediators
+##' @param Y outcome
+##' @param RCT TRUE indicates and RCT. This only changes how the parameter is labeled in the output...
+##' @param ... other arguments to be passed to tmle.cte
+##' @return an object of class "cte"
+##' @author Sam Lendle
+##' @export
 tmle.nde <- function(A, WZ, Y, RCT=FALSE, ...) {
   t <- tmle.cte(A, WZ, Y, a=0, ...)
   t$estimand <- ifelse(RCT,
