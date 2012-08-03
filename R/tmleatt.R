82a67f5e9ba5c6af6f43fd4db06ef4467be2024a
Sam Lendle <sam.lendle@gmail.com>
2012-08-03 13:39:10 -0700

    Update documentation and add option to estimate ATU

--- R/tmleatt.R
+++ R/tmleatt.R
@@ -1,17 +1,24 @@
-##' <description>
+##' Estimates the \strong{A}verage \strong{T}reatment effect among the \strong{T}reated (ATT) or \strong{U}ntreated (ATU), \emph{E[Y(1)-Y(O)|A=1]} or \emph{E[Y(1)-Y(0)|A=0]}, using targeted maximum likelihood.
 ##'
-##' <details>
-##' @title <title>
-##' @param Y outcome
-##' @param A treatment
-##' @param W baseline covariates
-##' @param ... other arguments to be passed to tmle.cte
-##' @return an object of class "cte"
-##' @author Sam Lendle
-##' @export
-tmle.att <- function(Y, A, W, ...) {
-  t <- tmle.cte(Y=Y, A=A, B=W, a=1, ...)
-  t$estimand <- "ATT"
+##' 
+##' @title TMLE for the ATT or ATU
+##' @param Y continuous or binary outcome variable
+##' @param A binary treatment indicator, \code{1} - treatment, \code{0} - control
+##' @param W vector, matrix, or dataframe containing baseline covariates to control for.
+##' @param param The parameter being estimated, "ATT" (default) or "ATU".
+##' @param ... other arguments passed to \code{\link{tmle.cte}}
+##' @return An object of class \code{tmle.cte} with \code{$estimand} set to \code{param}.
+##' @seealso \code{\link{tmle.cte}}
+##' @author Sam Lendle \email{lendle@@stat.berkeley.edu}
+tmle.att <- function(Y, A, W, param=c("ATT", "ATU"), ...) {
+  param <- match.arg(param)
+  if (param=="ATT") {
+    a <- 1
+  } else {
+    a <- 0
+  }
+  t <- tmle.cte(Y=Y, A=A, B=W, a=a, ...)
+  t$estimand <- param
   return(t)
 }
 
