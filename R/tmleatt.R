53faa587eabdf45a66c4b9fde534b672f8e4795e
Sam Lendle <sam.lendle@gmail.com>
2012-08-03 13:18:41 -0700

    Updated call to tmle.cte within tmle.att

--- R/tmleatt.R
+++ R/tmleatt.R
@@ -2,15 +2,15 @@
 ##'
 ##' <details>
 ##' @title <title>
+##' @param Y outcome
 ##' @param A treatment
 ##' @param W baseline covariates
-##' @param Y outcome
 ##' @param ... other arguments to be passed to tmle.cte
 ##' @return an object of class "cte"
 ##' @author Sam Lendle
 ##' @export
-tmle.att <- function(A, W, Y, ...) {
-  t <- tmle.cte(A, W, Y, a=1, ...)
+tmle.att <- function(Y, A, W, ...) {
+  t <- tmle.cte(Y=Y, A=A, B=W, a=1, ...)
   t$estimand <- "ATT"
   return(t)
 }
